1928be0f0edee694039190799b2586bd68af5da3
jesse <cyberdarko24@gmail.com>
2024-09-23 07:28:11 +0100

    Update

--- main.tf
+++ main.tf
@@ -1,6 +1,14 @@
 provider "aws" {
   region = "eu-west-2" # London region
 }
-resource "aws_s3_bucket" "terraformaugust230924" {
-  bucket ="terraformaugust230924"
+resource "aws_s3_bucket" "terraformaugusttest230924" {
+  bucket ="terraformaugusttest230924"
+}
+terraform {
+  backend "s3" {
+    # Replace this with your bucket name!
+    bucket         = "terraformaugusttest230924"
+    key            = "global/s3/terraform.tfstate"
+    region         = "eu-west-2"
+  }
 }
