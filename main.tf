6f6cfa3b436b3dfd0cb5b1114bc9ea4e158ec636
jesse <cyberdarko24@gmail.com>
2024-09-23 07:07:13 +0100

    updated s3 bucket name

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   region = "eu-west-2" # London region
 }
-resource "aws_s3_bucket" "terraformaugusttest" {
-  bucket ="terraformaugusttest"
+resource "aws_s3_bucket" "terraformaugust230924" {
+  bucket ="terraformaugust230924"
 }
