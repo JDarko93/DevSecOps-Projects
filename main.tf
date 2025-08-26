47a8833a67c071c0ab539477401cecbe5ffe23ca
jesse <cyberdarko24@gmail.com>
2025-08-26 17:02:52 +0100

    Updated S3 bucket name

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 provider "aws" {
   region = "eu-west-2" # London region
 }
-resource "aws_s3_bucket" "terraformaugusttest230924" {
-  bucket ="terraformaugusttest230924"
+resource "aws_s3_bucket" "terraformseptembertest260825" {
+  bucket ="terraformseptembertest260825"
 }
 terraform {
   backend "s3" {
     # Replace this with your bucket name!
-    bucket         = "terraformaugusttest230924"
+    bucket         = "terraformseptembertest260825"
     key            = "global/s3/terraform.tfstate"
     region         = "eu-west-2"
   }
