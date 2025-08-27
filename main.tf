7dcad553af9481e50a390ebb9b7ae7cbae2e12a4
jesse <cyberdarko24@gmail.com>
2025-08-27 22:03:43 +0100

    S3 bucket important

--- main.tf
+++ main.tf
@@ -7,8 +7,8 @@ resource "aws_s3_bucket" "terraformseptembertest260825" {
 terraform {
   backend "s3" {
     # Replace this with your bucket name!
-    bucket         = "terraformseptembertest260825"
-    key            = "global/s3/terraform.tfstate"
-    region         = "eu-west-2"
+  bucket         = "terraformseptembertest260825"
+  key            = "global/s3/terraform.tfstate"
+  region         = "eu-west-2"
   }
 }
